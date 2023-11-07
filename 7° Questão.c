6be9ff8b0fc6338ff9e0cfe7fee1d34aa115119b
uSiiLaS <142810322+uSiiLaS@users.noreply.github.com>
2023-11-06 21:02:42 -0300

    7° Questão.c

--- 7° Questão.c
+++ 7° Questão.c
@@ -8,32 +8,37 @@
 int main()
 {
    setlocale(LC_ALL, "portuguese");
-   
+
    int opcao;
+   int codigo;
    char resultado[250];
-   
+
    printf("\tMENU\t\n");
    printf("1\t|\tNOVO JOGO\n");
    printf("2\t|\tCARREGAR JOGO\n");
    printf("3\t|\tCONFIGURAÇÕES\n");
    printf("Informe o código com base no item desejado: \n");
    scanf("%d", &opcao);
-   
+
  system("cls || clear");  
-   
+
    switch (opcao){
        case 1:
-       strcpy(resultado, "Selecione a dificuldade do jogo:\n1-Fácil\n2-Normal\n3-Difícil\n4-Experiente");
+       printf("Selecione a dificuldade do jogo:\n1-Fácil\n2-Normal\n3-Difícil\n4-Experiente\n");
+         scanf("%d", &codigo);
+         system("cls || clear");
+         printf("Iniciando o jogo...");
        break;
        case 2:
-       strcpy(resultado, "°Carregar jogo 1'\n°Carregar jogo 2'\n°Carregar jogo 3'\n°Carregar jogo 4'\n");
+       printf("°Carregar jogo 1'\n°Carregar jogo 2'\n°Carregar jogo 3'\n°Carregar jogo 4'\n");
+         scanf("%d", &codigo);
+         system("cls || clear");
+         printf("Iniciando o jogo...");
        break;
        case 3:
-       strcpy(resultado, "°Controle\n°Vídeo\n°Áudio\n°Idioma\n°Fórum\n°Créditos");
+printf("°Controle\n°Vídeo\n°Áudio\n°Idioma\n°Fórum\n°Créditos");
        break;
    }
-   
-   printf("%s", resultado);
-   
+
     return 0;
 }
